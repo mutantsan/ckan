d0a623877720d4edfea021c34e5df52eae7d6761
mutantsan <kvaqich@gmail.com>
2025-08-06 20:35:17 +0300

    implement custom theme with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    darkMode: 'class',
+    content: [
+        './ckan/templates-tw/**/*.html',
+        './ckan/public-tw/base/css/**/*.css',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
